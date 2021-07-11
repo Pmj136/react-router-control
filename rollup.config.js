c05bfc8d2aefc6df3f32f998aa2c37e9bb707c56
PengMingJiu <pengmingjiu@gmail.com>
2021-07-11 12:58:15 +0800

    chore:update devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
-import commonjs from 'rollup-plugin-commonjs'
-import pkg from './package.json'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
 
 const env = process.env.NODE_ENV
 export default {
@@ -11,23 +10,27 @@ export default {
         {
             file: "build/index.esm.js",
             format: "esm",
-            name: "ReactRouterView",
             exports: "default"
         },
         {
             file: "build/index.cjs.js",
             format: "cjs",
-            name: "ReactRouterView",
             exports: "default"
         }
     ],
-    external: Object.keys(pkg.peerDependencies || {}),
+    external: [
+        "@babel/runtime",
+        "react",
+        "react-router-dom"
+    ],
     plugins: [
         nodeResolve(),
         babel({
-            exclude: '**/node_modules/**'
+            exclude: '**/node_modules/**',
+            babelHelpers: "runtime"
         }),
         replace({
+            preventAssignment: true,
             'process.env.NODE_ENV': JSON.stringify(env),
         }),
         commonjs()
