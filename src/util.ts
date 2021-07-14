4aeacf5c5a4c4e95e26c860ddeb3e1d83131e7be
PengMingJiu <pengmingjiu@gmail.com>
2021-07-14 17:51:43 +0800

    chore:update algorithm

--- src/util.ts
+++ src/util.ts
@@ -3,24 +3,23 @@ import {RouteItem} from "../index";
 export function resolveRoute(basePath: string, rawPath: string): string {
     if (rawPath.startsWith("/")) return rawPath
     if (basePath === '/') return "/" + rawPath
+    if (rawPath === "") return basePath
     return basePath + "/" + rawPath
 }
 
 export function specialRouteToEnd(routes: Array<RouteItem>) {
-    let allItem = null
-    const arrLen = routes.length;
-    let point = 0,
-        count = 0;
-    while (++count <= arrLen) {
-        if (routes[point].path === "*") {
-            allItem = routes[point]
-            routes.splice(point, 1)
-            continue
+    let index = 0,
+        p = -1
+    const len = routes.length
+    while (++p < len) {
+        if (routes[p].redirect !== undefined) {
+            index++
+        } else if (index != 0) {
+            [routes[p - index], routes[p]] = [routes[p], routes[p - index]]
         }
-        if (routes[point].redirect !== undefined) {
-            const moveItem = routes.splice(point, 1)[0];
-            routes.push(moveItem);
-        } else point++;
     }
-    if (allItem != null) routes.push(allItem)
+    const matchIndex = routes.findIndex(v => v.path === "*")
+    if (matchIndex !== -1 && matchIndex !== len - 1) {
+        routes.push(routes.splice(matchIndex, 1)[0])
+    }
 }
