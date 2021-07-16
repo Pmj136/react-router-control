08c713e946dfda76f03e95c108412a8f7e75d742
PengMingJiu <pengmingjiu@gmail.com>
2021-07-16 20:27:44 +0800

    chore:add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    rules: {
+        semi: ['error', 'never'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off', //使用 any 将警告
+    }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    semi: false,//不添加分号结尾
+    tabWidth: 4,
+    singleQuote: true
+}
